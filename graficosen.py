0b1401b9745a2b566ed44014fab3f90ed09d2f09
Elias Esquef <69909815+eliasnicolas91@users.noreply.github.com>
2020-08-20 08:44:15 -0300

    Add files via upload

---
+++ graficosen.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 10, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
+
+ 
---
+++ hello.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 14:04:06 2020
+
+@author: Elias Nicolas
+"""
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("prueba prueba")
+    
+    # Main program starts here
+hello()
+    
+
+
